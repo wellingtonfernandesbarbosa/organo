d3011ad41688a5306125987f467983acdde22f74
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-08-10 22:54:41 -0300

    Adição dos campos de formulário
    Neste commit, foram incorporados rótulos e entradas para os campos a seguir: "Nome", "Cargo" e "Imagem".

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,11 +1,9 @@
 import './Banner.css';
 
-function Banner() {
+export const Banner = () => {
     return (
         <header className='banner'>
             <img src="/imagens/banner.png" alt="O banner principal da página do Organo"></img>
         </header>
     )
 }
-
-export default Banner;
