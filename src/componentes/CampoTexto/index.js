7b3894c0f17d08e50440f720ad5e7a9ae93f59d7
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2023-08-11 22:31:52 -0300

    Adição da manipulação das informações do formulário Neste commit, foi aprimorada a manipulação dos dados do formulário ao implementar o tratamento dos mesmos usando a função useState do React. Isso resultou na criação de estados individuais para campos como nome, cargo, imagem e time, permitindo um controle reativo e eficiente das informações. Além disso, o código foi otimizado ao centralizar a lógica de atualização dos estados em uma função aoAlterarCampo, tornando-o mais organizado e modular.

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,17 @@
 import './CampoTexto.css';
 
 const CampoTexto = (props) => {
+
+    const placeholdModificada = `${props.placeholder}...`
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+    
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input placeholder={props.placeholder} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholdModificada} />
         </div>
     );
 }
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,7 +4,11 @@ const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.obrigatorio}>{props.itens.map(item => <option key={item}>{item}</option>)}</select>
+            <select 
+                onChange={evento => props.aoAlterado(evento.target.value)}
+                required={props.obrigatorio}>
+                    {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
         </div>
     )
 };
