5a228b0cb588f0d42706438b864607336b102672
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-08-21 23:29:52 -0300

    Reforço da Robustez e Aprimoramento Visual no Componente Colaborador
    Neste commit, fortaleci a implementação do componente Colaborador ao introduzir uma chave, ampliando a robustez do código. Além disso, otimizei a experiência visual ao ajustar o tamanho da imagem do colaborador de forma a garantir uma visualização mais agradável

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,8 +5,9 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select 
-                onChange={evento => props.aoAlterado(evento.target.value)}
-                required={props.obrigatorio} value={props.valor}>
+                onChange={evento => props.aoAlterado(evento.target.value)} 
+                required={props.obrigatorio} 
+                value={props.valor}>
                 <option value=""></option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
